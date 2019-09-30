01836ef7462868e967b038dd6639057b1af2fc29
pbillerot <philippe.billerot@gmail.com>
2019-09-30 11:09:48 +0200

    résolution conflits

--- player/player.py
+++ player/player.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, Blueprint
-from flask import request, session
-import pygame
 import time
 import os, logging
 
-def playMediafile(filePath):
-  if session["player"] is not None:
-    session["player"] = pygame.mixer.Sound(filePath)
-  else:
-    logging.info(f"stop")
-    session["player"].stop()
-    session["player"] = pygame.mixer.Sound(filePath)
-  logging.info(f"play: {filePath}")
-  session["player"].play(-1)
-
 player = Blueprint('main', __name__, url_prefix="/player")
-#Initialisation Pygame
-pygame.mixer.init()
 
 @player.route('/')
 def accueil():
@@ -26,19 +12,21 @@ def accueil():
 @player.route('/play/<mediafile>')
 def play(mediafile):
   filePath = os.path.join(player.root_path, 'media', mediafile)
-  playMediafile(filePath)
-  return f'Media: play {mediafile}\n'
+  command = f"audacious {filePath}"
+  logging.info(command)
+  os.system(command)
+  return f'Play {command}\n'
 
 @player.route('/stop')
 def stop():
-  if session["player"] is not None:
-    logging.info(f"stop")
-    session["player"].stop()
-  return 'Media: stop\n'
+  logging.info("stop")
+  os.system(f"audacious --stop")
+  return 'Stop: current\n'
 
 # Tests unitaires
 if __name__ == '__main__':
   # global app
   app = Flask(__name__)
   app.register_blueprint(player)
+  logging.info("Player en marche...")
   app.run()
