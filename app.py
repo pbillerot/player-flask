db0a3bf6fd3111b02a296d12bbb92dbdf550cb2b
pbillerot <philippe.billerot@gmail.com>
2019-10-05 18:09:56 +0200

    Nettoyage

--- app.py
+++ app.py
@@ -5,26 +5,6 @@ from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
-# création de l'objet logger qui va nous servir à écrire dans les logs
-#logger = logging.getLogger()
-# on met le niveau du logger à DEBUG, comme ça il écrit tout
-# logger.setLevel(logging.DEBUG)
-# création d'un formateur qui va ajouter le temps, le niveau
-# de chaque message quand on écrira un message dans le log
-# formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
-# création d'un handler qui va rediriger une écriture du log vers
-# un fichier en mode 'append', avec 1 backup et une taille max de 5Mo
-# file_handler = RotatingFileHandler('log/player.log', 'a', 5000000, 1)
-# on lui met le niveau sur DEBUG, on lui dit qu'il doit utiliser le formateur
-# créé précédement et on ajoute ce handler au logger
-# file_handler.setLevel(logging.DEBUG)
-# file_handler.setFormatter(formatter)
-# logger.addHandler(file_handler)
-# création d'un second handler qui va rediriger chaque écriture de log sur la console
-# stream_handler = logging.StreamHandler()
-# stream_handler.setLevel(logging.DEBUG)
-# logger.addHandler(stream_handler)
-
 @app.route('/')
 def accueil():
   message = "Player à votre écoute..."
